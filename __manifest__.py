234bc0e74ab4c217d07452924008a33c36e444e5
Zarcam <zarcam.t@gmail.com>
2024-02-22 18:18:42 +0100

    Modificado: Informacion del manifest

--- __manifest__.py
+++ __manifest__.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "gestion_ciclos_instituto",
+    'name': "Gestion Ciclos Instituto",
 
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
+    'summary': "Gestion de ciclos, modulos, alumnos y profesores",
 
     'description': """
-Long description of module's purpose
+        Modulo para la gestion de ciclos, modulos, alumnos y profesores de un instituto
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "Alejandro Rebagliato García",
+    'website': "https://github.com/Zarcam/Tarea04-GestionCiclosInstituto-SGE",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Administration',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
